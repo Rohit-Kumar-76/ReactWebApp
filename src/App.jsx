0c487ed3071a758524f3228d513363d4ffc96c08
Rohit Kumar <rohit.jit76@gmail.com>
2025-08-29 16:05:49 +0530

    Router update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Service from './Pages/Service'
@@ -16,17 +16,19 @@ import HeroSlider from './Components/HeroSlider'
 const App = () => {
   return (
     <>
-      <Navbar />
-      {/* <Main /> */}
-      <HeroSlider />
+      <Router>
+        <Navbar />
+        {/* <Main /> */}
+        <HeroSlider />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/service" element={<Service />} />
-      </Routes>
-      <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
